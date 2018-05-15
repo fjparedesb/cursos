91d61fb56d970bcc4b2fb0c60d6c8480ea002d24
Javier Paredes <fjparedesb@gmail.com>
2018-05-15 17:16:54 -0500

    clase de parametros

--- nodejs/notes-node/app.js
+++ nodejs/notes-node/app.js
@@ -5,3 +5,16 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');
 
+var command = process.argv[2];
+
+if (command === 'add'){
+	console.log('Adding a note');
+} else if (command === 'list') {
+	console.log('Listing all notes');
+} else if (command === 'read'){
+	console.log('Reading a note');
+} else if (command === 'remove'){
+	console.log('Deleting a note');
+} else {
+	console.log('Command not recognized');
+}
