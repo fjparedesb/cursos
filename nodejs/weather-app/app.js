a6ed96e3b3724256b6ceb4f36ca09d561f32e10f
Javier Paredes <fjparedesb@gmail.com>
2018-06-04 23:35:54 -0500

    curso node

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -1,8 +1,30 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombardi%20street%20philadelphia',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.a)}`,
     json: true
 }, (error, response, body) => {
-        console.log(JSON.stringify(body, undefined, 2));
+        if (error) {
+            console.log('No se pudo conectar a los servidores de Google');
+        } else if (body.status !== "OK") {
+            console.log('No se encontraron direcciones validas');
+        } else {
+            console.log(`Adress: ${body.results[0].formatted_address}`);
+            console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+            console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+        }
 });
